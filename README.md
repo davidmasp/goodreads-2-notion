3083fcabef0e2c0c8b20145e9fc42668170dc41e
david mas <david.mas.p@gmail.com>
2021-09-20 11:54:35 +0200

    :construction: update

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # goodreads-2-notion
-api integration to move your exported library to notion
+
+Api integration to move your exported library to notion
+
+## Usage
+
+Goodreads api is not open now apparently (see [here](https://help.goodreads.com/s/article/Does-Goodreads-support-the-use-of-APIs)).
+
+The way to obtan your lists is to go to to > `My Books` and then
+go to `Import/Export`, then press into `Export Library`.
+
+## Dev
+
+Use this to use node in powershell -> `$env:Path += ";C:\Program Files\nodejs\"`
+. From [here](https://stackoverflow.com/questions/19569990).
---
+++ index.js
@@ -0,0 +1,115 @@
+
+// this captures env info from the .env file
+require('dotenv').config()
+// vars needed
+// NOTION_TOKEN
+
+// imports
+const fs = require('fs');
+const path = require('path');
+const csv = require('fast-csv');
+
+// start the client
+const { Client } = require("@notionhq/client");
+const { runInThisContext } = require('vm');
+
+// Initializing a client
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+})
+
+
+function parseBookRow(row) {
+  const bookObjRes = {}
+  bookObjRes.id = row["Book Id"]
+  bookObjRes.title = row["Title"]
+  bookObjRes.author = row["Author"]
+  bookObjRes.isbn = row["ISBN"]
+  bookObjRes.publisher = row["Publisher"]
+  bookObjRes.author2 = row["Author l-f"]
+  bookObjRes.type = row["Exclusive Shelf"]
+  bookObjRes.avgR = row["Average Rating"]
+  bookObjRes.dateAdded = row["Date Added"]
+  bookObjRes.dateRead = row["Date Read"]
+  bookObjRes.publicationYear = row["Original Publication Year"]
+  bookObjRes.numPages = row["Number of Pages"]
+  bookObjRes.myRating = row["My Rating"]
+  return bookObjRes
+}
+
+async function addPageToDb(dbid, bookObj) {
+  return new Promise((resolve, reject) => {
+    setTimeout(async () => {
+      const book_icons = ["📗", "📘", "📙", "📕", "📚", "📖"];
+      const icon_chosen = book_icons[Math.floor(Math.random() * book_icons.length)];
+      //console.log(bookObj);
+      obj_to_create = {
+        parent: {
+          database_id: dbid,
+        },
+        icon: {
+          type: "emoji",
+          emoji: icon_chosen,
+        },
+        properties: {
+          Name: {
+            title: [
+              {
+                text: {
+                  content: bookObj.title,
+                },
+              },
+            ],
+          },
+          Type: {
+            select: {
+              name: bookObj.type,
+            },
+          },
+          Pages: {
+            number: parseInt(bookObj.numPages, 10),
+          },
+          Author: {
+            select: {
+              name: bookObj.author,
+            }
+          },
+          AvgR: {
+            number: parseFloat(bookObj.avgR),
+          },
+          Year: {
+            number: parseInt(bookObj.publicationYear, 10),
+          },
+        }
+      }
+      try {
+        var response = await notion.pages.create(obj_to_create);
+        resolve(response);
+        //console.log(response);
+      } catch (err) {
+        console.log(err);
+      }
+    }, 500);
+  });
+  
+}
+
+const run = async () => {
+  console.log('==> Reading csv ...');
+  const books_list = [];
+  const readStream = fs.createReadStream(path.resolve(__dirname, 'data', 'goodreads_library_export.csv'))
+    .pipe(csv.parse({ headers: true }))
+    .on('error', (error) => console.error('stream error: ', error))
+    .on('data', (row) => {
+      const dat_book = parseBookRow(row);
+      books_list.push(dat_book);
+    })
+    .on('end', async rowCount => {
+      for (let i = 0; i < books_list.length; i++) {
+        await addPageToDb(process.env.DB_ID,books_list[i]);
+      }
+      console.log('==> End of script')
+    })
+}
+
+run();
